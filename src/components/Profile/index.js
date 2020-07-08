936a1c171b225f975129fb5f90fdf36a590e0a9e
Natália Azevedo <naaatalia.azevedo@live.com>
2020-07-07 23:03:03 -0300

    create component about profile

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>matt Foo</h1>
+    <h2>Programador Backend</h2>
+    <p>La nasndksd asdmkasd askdkasd kasdksja kasjdkasj kajsdkja akjsdkaj  aksjdkajdkj</p>
+  </div>
+);
+
+export default Profile;

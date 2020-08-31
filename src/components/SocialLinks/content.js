553493b31818049ce6c3652074adf3cb88e991eb
Natália Azevedo <naaatalia.azevedo@live.com>
2020-08-30 21:41:58 -0300

    feat(links): add links social and links page

--- src/components/SocialLinks/content.js
+++ src/components/SocialLinks/content.js
@@ -1,24 +1,24 @@
 const links = [
-  // {
-  //   label: "Github",
-  //   url: "https://github.com/naaataliaazevedo",
-  // },
-  // {
-  //   label: "Twitter",
-  //   url: "https://twitter.com/naaataliaazevedo",
-  // },
-  // {
-  //   label: "Youtube",
-  //   url: "https://github.com/naaataliaazevedo",
-  // },
-  // {
-  //   label: "Instagram",
-  //   url: "https://github.com/naaataliaazevedo",
-  // },
-  // {
-  //   label: "Unsplash",
-  //   url: "https://github.com/naaataliaazevedo",
-  // },
+  {
+    label: "Github",
+    url: "https://github.com/naaataliaazevedo",
+  },
+  {
+    label: "Twitter",
+    url: "https://twitter.com/naaataliaazevedo",
+  },
+  {
+    label: "Youtube",
+    url: "https://github.com/naaataliaazevedo",
+  },
+  {
+    label: "Instagram",
+    url: "https://github.com/naaataliaazevedo",
+  },
+  {
+    label: "Unsplash",
+    url: "https://github.com/naaataliaazevedo",
+  },
 ]
 
 export default links;

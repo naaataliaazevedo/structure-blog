2c9c40bfe1abc978dc6b9e8aa6428c794ba039c0
Natália Azevedo <naaatalia.azevedo@live.com>
2020-09-01 22:40:27 -0300

    feat(node): using node api to add new field to slug

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,9 @@ const IndexPage = () => {
               title
             }
             timeToRead
+            fields {
+              slug
+            }
           }
         }
       }
@@ -35,10 +38,13 @@ const IndexPage = () => {
           node: {
             frontmatter: { background, category, date, description, title },
             timeToRead,
+            fields: {
+              slug
+            },
           },
         }) => (
           <PostItem
-            slug="/about/"
+            slug={slug}
             background={background}
             category={category}
             date={date}

5ea7c9f12ecc94818440ca376a6299c27ea665cf
Natália Azevedo <naaatalia.azevedo@live.com>
2020-09-28 22:16:22 -0300

    feat(algolia): add configuration algolia inside the project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config()
+
+const queries = require('./src/utils/algolia.queries')
+
 module.exports = {
   siteMetadata: {
     title: `My Blog`,
@@ -48,12 +52,23 @@ module.exports = {
             },
           },
           `gatsby-remark-lazy-load`,
-          `gatsby-remark-prismjs`
+          `gatsby-remark-prismjs`,
         ],
       },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        indexName: process.env.ALGOLIA_INDEX_NAME,
+        queries,
+        chunkSize: 10000,
+        enablePartialUpdates: true,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
---
+++ src/utils/algolia.queries.js
@@ -0,0 +1,44 @@
+const postsQuery = `{
+  posts: allMarkdownRemark(
+    sort: { fields: frontmatter___date, order: DESC }
+  ) {
+    edges {
+      node {
+        objectID: id
+        frontmatter {
+          date_timestamp: date
+          category
+          date(formatString: "DD [de] MMMM [de] YYYY", locale: "pt-br")
+          description
+          title
+        }
+        fields {
+          slug
+        }
+        excerpt(pruneLength: 5000)
+      }
+    }
+  }
+}`
+
+const flatten = arr =>
+  arr.map(({ node: { frontmatter, ...rest } }) => ({
+    ...frontmatter,
+    date_timestamp: parseInt(
+      (new Date(frontmatter.date_timestamp).getTime() / 1000).toFixed(0)
+    ),
+    ...rest,
+  }))
+
+const queries = [
+  {
+    query: postsQuery,
+    transformer: ({ data }) => flatten(data.posts.edges),
+    indexName: "prod_BLOG",
+    settings: {
+      attributesToSnippet: ["excerpt:20"],
+    },
+  },
+]
+
+module.exports = queries

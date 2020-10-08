a0df12033b471bf666dd3ef5479e886b707bc6cd
Natália Azevedo <naaatalia.azevedo@live.com>
2020-10-08 12:42:28 -0300

    feat(media): add media query and lib transition link

--- src/components/RecommendedPosts/styled.js
+++ src/components/RecommendedPosts/styled.js
@@ -1,9 +1,10 @@
 import styled from "styled-components"
+import media from "styled-media-query"
 import { Link } from "gatsby"
 
 export const RecommendedWrapper = styled.section`
   border-bottom: 1px solid var(--borders);
-  border-top: 1px solid #var(--borders);
+  border-top: 1px solid var(--borders);
   background: var(--mediumBackground);
   display: flex;
 `
@@ -17,6 +18,13 @@ export const RecommendedLink = styled(Link)`
   text-decoration: none;
   transition: background 0.5s;
   width: 50%;
+
+  ${media.lessThan("large")`
+    padding: 2rem 1rem;
+    line-height: 1.3;
+    font-size: .9rem;
+  `}
+
   &:hover {
     background: var(--borders);
   }
